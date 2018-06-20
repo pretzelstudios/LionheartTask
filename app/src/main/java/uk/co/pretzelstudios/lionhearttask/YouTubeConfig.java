2b1dac4542fedb53beda4ddb576a5bd8bcdd04e7
xsocomx <36047773+xsocomx@users.noreply.github.com>
2018-06-21 00:08:08 +0100

    I wanted to add video play back into my app directly from Youtubes API to enable a smooth end user experience. I need to change "extends YoutubeBaseActivity" back to "extends AppCompatActivity" in my FronPageActivity as this interfiers with my tool bar so I need to add the player in a Fragment to overcome this.

---
+++ app/src/main/java/uk/co/pretzelstudios/lionhearttask/YouTubeConfig.java
@@ -0,0 +1,11 @@
+package uk.co.pretzelstudios.lionhearttask;
+
+public class YouTubeConfig {
+    public YouTubeConfig() {
+    }
+    private static  final String API_KEY = "AIzaSyAJbM5mbsYu0mUzlGbMZ-2JPjy9ty5Clo4";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
